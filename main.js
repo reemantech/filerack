4ee7ede793933d2fff4acd9748e92da2df781b0e
Shivam Kumar <98273838+reemantech@users.noreply.github.com>
2023-07-07 19:03:31 +0530

    tree representation completed

--- main.js
+++ main.js
@@ -30,8 +30,45 @@ switch (command) {
 }
 
 function treeFn(dirPath) {
-  console.log("tree command implemented");
+//   console.log("tree command implemented");
+    if (dirPath == undefined) {
+        errorHandler();
+        return;
+    } else {
+        // -check for the valid current path
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+            treeHelper(dirPath,"");
+        } else {
+            errorHandler.invalidPath();
+            return;
+        }
+    }
+
 }
+
+function treeHelper(dirPath, indent){
+    let isFile = fs.lstatSync(dirPath).isFile();
+
+    if(isFile == true){
+        let fileName = path.basename(dirPath);
+        console.log(indent+"├──" + fileName );
+    }else{
+        let dirName = path.basename(dirPath);
+        console.log(indent+"└──"+ dirName);
+
+        let childrens = fs.readdirSync(dirPath);
+
+        for(let i=0;i<childrens.length; i++){
+            let childPath = path.join(dirPath, childrens[i]);
+            treeHelper(childPath, indent+"\t");
+        }
+    }
+}
+
+
+
+
 
 function organizeFn(dirPath) {
   //   console.log("organise command implemented");
@@ -112,12 +149,6 @@ function getCategory(fileName){
 
     return "others";
 }
-
-
-
-
-
-
 
 
 
