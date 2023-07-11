b900bd9c297621c197e5d4b293f1c8a4dd5576df
Shivam Kumar <98273838+reemantech@users.noreply.github.com>
2023-07-11 12:30:33 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,10 @@
 function helpFn() {
   console.log(`
   syntax:\tfilerack command _directoryPath_\n
-  commands listed below:\n
-          organize \n
-          tree \n
-          help\n
+  commands listed below:\t description\n
+          organize\t - organize the files in a folder in the current working directory(by default) or in specified directory\n 
+          tree\t - display the tree representation of the files and folders in terminal\n
+          help\t - self explanatory\n
       `);
 }
 
